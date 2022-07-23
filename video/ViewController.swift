b0324b7cfade078cf2d3de057e34d674e01d0e28
Kashish Sharma <kashish6011@gmail.com>
2022-07-24 01:42:45 +0530

    Initial Commit

---
+++ video/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video
+//
+//  Created by Kashish Sharma on 24/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
